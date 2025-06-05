28cca656f1053112bf8e1bc2a7cb5cf3279eee30
Kerry Powell <1776069+powellkerry@users.noreply.github.com>
2025-06-05 15:42:08 -0600

    update site root

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/powellkerry.github.io/',
+    base: 'https://powellkerry.github.io/',
     plugins: [react()],
     css: {
         postcss: './postcss.config.js',
